ca5efe452cbe2631596c677dae6cee9c4e440a5f
Yeray <comodin26@gmail.com>
2019-05-24 13:58:40 +0200

    Clase creada, version inicial del paso 3

---
+++ ejercicio6/src/ejercicio6/Casa.java
@@ -0,0 +1,19 @@
+package ejercicio6;
+
+public class Casa {
+
+	private float superficie;
+	private String calle;
+	private int codigoPostal;
+	
+	/**
+	 * Un equipo de limpiadores deja la casa libre de gérmenes
+	 * y desinfecta todas las superficies
+	 **/
+	
+	public void limpiarLaCasa(){
+		String mensajito="Te hemos limpiado la casa, está desinfectada";
+		System.out.println(mensajito);
+	}
+	
+}
